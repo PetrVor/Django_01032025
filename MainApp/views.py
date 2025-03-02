46acd15d31be82c35a0d23f57acccbe0e24ee89b
PetrVor <ninja-xiii@mail.ru>
2025-03-02 19:48:01 +0300

    Completed Module1:task 3(items)

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 
 
 author = {
@@ -9,6 +9,15 @@ author = {
     "телефон": "8-923-666-03-66",
     "email": "Eugen@mail.ru"
     }
+
+items = [
+   {"id": 1, "name": "Кроссовки abibas", "quantity" : 5},
+   {"id": 2, "name": "Куртка кожаная", "quantity" : 2},
+   {"id": 3, "name": "Coca-cola 1 литр", "quantity" : 12},
+   {"id": 4, "name": "Картофель фри", "quantity" : 0},
+   {"id": 5, "name": "Кепка", "quantity" : 124},
+]
+
 
 def home(request):
     text =""" 
@@ -25,5 +34,14 @@ def about(request):
     "телефон": <b>{author['телефон']}</b><br>
     "email": <b>{author['email']}</b><br>
     """
-
     return HttpResponse(text)
+
+def get_item(request,item_id):
+    for item in items:
+        if item['id'] == item_id:
+            res = f"""
+            <h2>имя: {item["name"]} </h2>
+            <p> количество: {item["quantity"]}</p>
+            """
+            return HttpResponse(res)
+    return HttpResponseNotFound(f"Item with id={item_id} not found!")    
