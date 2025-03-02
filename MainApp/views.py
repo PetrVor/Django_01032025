d1b3e89b76f7f6c6c5a96b34b372a40f624e8e3e
PetrVor <ninja-xiii@mail.ru>
2025-03-02 18:55:18 +0300

    Done Modeule1: tasks 1,2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+author = {
+    "Имя": "Евген",
+    "Отчество" : "Борисович",
+    "Фамилия": "Шершнев",
+    "телефон": "8-923-666-03-66",
+    "email": "Eugen@mail.ru"
+    }
+
+def home(request):
+    text =""" 
+    <h1>"Изучаем django"</h1>
+    <strong>Автор</strong>: <i>Воробьев П.С.</i>
+    """
+    return HttpResponse(text)
+
+def about(request):
+    text=f"""
+    "Имя": <b>{author['Имя']}</b><br>
+    "Отчество" : "<b>{author['Отчество']}</b><br>
+    "Фамилия": <b>{author['Фамилия']}</b><br>
+    "телефон": <b>{author['телефон']}</b><br>
+    "email": <b>{author['email']}</b><br>
+    """
+
+    return HttpResponse(text)
