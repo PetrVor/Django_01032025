03e0739411e7b3476f2387dcd86316223a8265f5
Petr Vorr <ninja-xiii@mail.ru>
2025-03-10 21:23:16 +0300

    Complete Modeule 3 tasks 3-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,24 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound
-
 from MainApp.models import Item
-
-
-# author = {
-#     "name": "Евген",
-#     "mid_name" : "Борисович",
-#     "last_name": "Шершнев",
-#     "phone": "8-923-666-03-66",
-#     "email": "Eugen@mail.ru"
-#     }
-
-# items = [
-#    {"id": 1, "name": "Кроссовки abibas", "quantity" : 5},
-#    {"id": 2, "name": "Куртка кожаная", "quantity" : 2},
-#    {"id": 3, "name": "Coca-cola 1 литр", "quantity" : 12},
-#    {"id": 4, "name": "Картофель фри", "quantity" : 0},
-#    {"id": 5, "name": "Кепка", "quantity" : 124},
-# ]
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def home(request):
@@ -40,28 +23,18 @@ def about(request):
     
     return render(request,"about.html", {"author": author})
 
-def get_item(request,item_id):
-    #вариант без генератора
-    # for item in items:
-    #     if item['id'] == item_id:
-    #             context={"item": item}
-    #     else:
-    #          continue  
-    #     return render(request,"item_page.html", context)    
-    # return HttpResponseNotFound(f"Item with id={item_id} not found!")    
-
-    item = next((item for item in items if item['id'] == item_id), None)
-    if item is not None:
-        context = {
-            "item": item
-        }
+def get_item(request,item_id: int):
+    try:
+        item = Item.objects.get(id =item_id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Item with id={item_id} not found!")
+    else:
+        context = {"item": item}
         return render(request,"item_page.html", context)    
-    return HttpResponseNotFound(f"Item with id={item_id} not found!")
+    
 
 
 def get_items(request):
     items = Item.objects.all()
-    context={
-        "items": items
-    } 
+    context={"items": items} 
     return render(request,"items_list.html", context)
