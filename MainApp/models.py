3b39c2467ac408d845d322efd6515e4cc81d96c6
Petr Vorr <ninja-xiii@mail.ru>
2025-03-14 20:27:36 +0300

    Add fixtures for colors

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+class Color(models.Model):
+   name=models.CharField(max_length=32)
+
+   def __repr__(self):
+      return f"Color({self.name})"
+
 class Item(models.Model):
    name  = models.CharField(max_length=100)
    brand = models.CharField(max_length=100)
    count = models.PositiveIntegerField()
    description = models.TextField(max_length=200, default='Base item description') 
+   colors = models.ManyToManyField(to=Color)
+
+   def __repr__(self):
+      return f"Item({self.name})"
 
